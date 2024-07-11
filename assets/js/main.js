9232b1eee095eb7a32078213c09d595dff823982
Subhoshri Pal <138369802+Subhoshri@users.noreply.github.com>
2024-07-11 10:17:11 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ const blurHeader = () =>{
     this.scrollY >= 50 ? header.classList.add('blur-header') 
                        : header.classList.remove('blur-header')
 }
-window.addEventListener('scroll', blurHeaderlurHeader)
+window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 const scrollUp = () =>{
